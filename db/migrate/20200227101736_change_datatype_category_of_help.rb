7c0522277b0ce6940b4b7deaa87c8dc08997431b
Giftport <info@giftport.jp>
2020-04-24 11:42:43 +0900

    デプロイまでの設定変更1

--- db/migrate/20200227101736_change_datatype_category_of_help.rb
+++ db/migrate/20200227101736_change_datatype_category_of_help.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypeCategoryOfHelp < ActiveRecord::Migration[5.2]
   def change
+    #development
+  	#change_column :helps, :category, :integer
+
+    #production
   	change_column :helps, :category, 'integer USING CAST(category AS integer)'
   end
 end
--- db/migrate/20200315155729_change_datatype_offer_status_of_offers.rb
+++ db/migrate/20200315155729_change_datatype_offer_status_of_offers.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypeOfferStatusOfOffers < ActiveRecord::Migration[5.2]
   def change
+    #development
+    #change_column :offers, :offer_status, :integer
+
+    #production
     change_column :offers, :offer_status, 'integer USING CAST(offer_status AS integer)'
   end
 end
