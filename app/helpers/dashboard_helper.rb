494e08155de3b6494fb64ce48210edfb863c7845
Giftport <info@giftport.jp>
2020-05-03 23:26:18 +0900

    ダッシュボードの修正

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,17 +1,12 @@
 module DashboardHelper
-  def show_reward(performer)
-    salary = 0
+  def show_reward(livehouse)
+    #月の設定
     beginning_of_month = DateTime.now.beginning_of_month
     end_of_month = DateTime.now.end_of_month
-  	offers = Offer.where('offered_performer = ? AND (meeting_time BETWEEN ? AND ?)', performer, beginning_of_month, end_of_month)
-    offers.each do |i|
-      if i.payment.present?
-        salary += (i.payment.guarantee + i.payment.withholding_tax)
-      else
-        salary += 0
-      end
-    end
-    return offers.length, salary
+    #支援金の計算
+  	donations = Donation.where('livehouse_id = ? AND (created_at BETWEEN ? AND ?)', livehouse, beginning_of_month, end_of_month)
+    donation_total = donations.sum(:amount)
+    donation_recievable = donation_total * ( 1 - Constants::SYSTEM_FEE )
+    return donations.length, donation_total, donation_recievable.to_i
   end
-
 end
