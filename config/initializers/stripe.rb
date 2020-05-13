bc3bddb4d3843b7f577abe16dfa5f685b766e469
Giftport <info@giftport.jp>
2020-05-13 23:20:20 +0900

    STRIPE OAUTH IDのCredentials登録

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,16 @@
 if Rails.env.development? || Rails.env.test?
   Rails.configuration.stripe = {
     publishable_key: Rails.application.credentials.STRIPE_PUBLISHABLE_KEY_DEV,
-    secret_key: Rails.application.credentials.STRIPE_SECRET_KEY_DEV
+    secret_key: Rails.application.credentials.STRIPE_SECRET_KEY_DEV,
+    oauth_client_id: Rails.application.credentials.STRIPE_OAUTH_CLIENT_ID_DEV
   }
 end
 
 if Rails.env.production?
   Rails.configuration.stripe = {
     publishable_key: Rails.application.credentials.STRIPE_PUBLISHABLE_KEY,
-    secret_key: Rails.application.credentials.STRIPE_SECRET_KEY
+    secret_key: Rails.application.credentials.STRIPE_SECRET_KEY,
+    oauth_client_id: Rails.application.credentials.STRIPE_OAUTH_CLIENT_ID
   }
 end
 
