a394d4904543249856a135d65bbae11ee7222b82
Giftport <info@giftport.jp>
2020-06-18 14:49:55 +0900

    会社名に変更

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,10 +1,23 @@
 class SearchsController < ApplicationController
   def index
-    keyword = params[:keyword]
-    @livehouses = Livehouse.where(
-      ['livehouse_name LIKE? or area LIKE? or genre LIKE?',
-        "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"]
-      ).where(certified: true).where(published: true).page(params[:page]).per(30)
+    #sort = params[:sort] || "created_at DESC"
+    @keyword = params[:keyword]
+
+    if @keyword.present?
+      @livehouses = []
+
+      @keyword.split(/[[:blank:]]+/).each do |keyword|
+        next if keyword == ""
+        @livehouses += Livehouse.where(
+          ['livehouse_name LIKE? or area LIKE? or genre LIKE?',
+            "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"]
+          ).where(certified: true, published: true).order("created_at DESC").page(params[:page]).per(30)
+        @livehouses.uniq!
+      end
+
+    else
+      @livehouses = Livehouse.order("created_at DESC").limit(30).page(params[:page]).per(30)
+    end
   end
 
 private
